0464398b2c3934cc376ec6f7479d8e6524f8c4b2
Subliminal Guy <subliminal_kid@posteo.de>
2023-03-07 18:33:33 +0100

    Add Van Detail for real

---
+++ src/components/Header.jsx
@@ -0,0 +1,30 @@
+import { Link, NavLink} from "react-router-dom"
+
+
+export default function Navbar() {
+    const activeStyle = {
+        "fontWeight": "bold",
+        "textDecoration": "underline",
+        "color": "#161616"
+    }
+
+    return (
+        <header>
+            <Link className="site-logo" to="/">#VanLife</Link>
+                <nav>
+                    <NavLink 
+                    to="/host"
+                    style={({isActive}) => isActive ? activeStyle : null}
+                    >Host</NavLink>
+                    <NavLink 
+                    to="/vans"
+                    style={({isActive}) => isActive ? activeStyle : null}
+                    >Vans</NavLink>
+                    <NavLink 
+                    to="/about"
+                    style={({isActive}) => isActive ? activeStyle : null}
+                    >About</NavLink>
+                </nav>
+        </header>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,18 @@ import { BrowserRouter, Routes, Route} from "react-router-dom"
 
 import Home from "./pages/Home"
 import About from "./pages/About"
-import Navbar from "./pages/Navbar"
-import Vans from "./pages/Vans"
-import VanDetail from "./pages/VanDetail"
+import Vans from "./pages/Vans/Vans"
+import VanDetail from "./pages/Vans/VanDetail"
+import Layout from "./components/Layout"
+import HostLayout from "./components/HostLayout"
+
+import Dashboard from "./pages/Host/Dashboard"
+import Income from "./pages/Host/Income"
+import Reviews from "./pages/Host/Reviews"
+import HostVans from "./pages/Host/HostVans"
+import HostVanDetail from "./pages/Host/HostVanDetail"
+
+
 
 import "./index.css";
 import "./server"
@@ -15,12 +24,21 @@ import "./server"
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/vans" element={<Vans />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vans/:id" element={<VanDetail />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="vans" element={<Vans />} />
+          <Route path="vans/:id" element={<VanDetail />} />
+          
+          <Route path="host" element={<HostLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="income" element={<Income />} />
+            <Route path="vans" element={<HostVans />} />
+            <Route path="vans/:id" element={<HostVanDetail />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   )
