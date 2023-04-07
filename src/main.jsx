cf5817220ba184bae1894112291c0d9f903e2ef2
Subliminal Guy <subliminal_kid@posteo.de>
2023-04-07 10:17:23 +0200

    Add specific redirect

--- src/main.jsx
+++ src/main.jsx
@@ -63,17 +63,17 @@ const router = createBrowserRouter(
                 <Route
                     index
                     element={<Dashboard />}
-                    loader={async () => await requireAuth()}
+                    loader={async ({ request }) => await requireAuth(request)}
                 />
                 <Route
                     path="income"
                     element={<Income />}
-                    loader={async () => await requireAuth()}
+                    loader={async ({ request }) => await requireAuth(request)}
                 />
                 <Route
                     path="reviews"
                     element={<Reviews />}
-                    loader={async () => await requireAuth()}
+                    loader={async ({ request }) => await requireAuth(request)}
                 />
                 <Route
                     path="vans"
@@ -88,17 +88,23 @@ const router = createBrowserRouter(
                     <Route
                         index
                         element={<HostVanInfo />}
-                        loader={async () => await requireAuth()}
+                        loader={async ({ request }) =>
+                            await requireAuth(request)
+                        }
                     />
                     <Route
                         path="photos"
                         element={<HostVanPhotos />}
-                        loader={async () => await requireAuth()}
+                        loader={async ({ request }) =>
+                            await requireAuth(request)
+                        }
                     />
                     <Route
                         path="pricing"
                         element={<HostVanPricing />}
-                        loader={async () => await requireAuth()}
+                        loader={async ({ request }) =>
+                            await requireAuth(request)
+                        }
                     />
                 </Route>
             </Route>
--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,11 @@
 import { redirect } from 'react-router-dom'
 
-export async function requireAuth() {
+export async function requireAuth(request) {
+    const pathname = new URL(request.url).pathname
     const isLoggedIn = localStorage.getItem('loggedin')
     if (!isLoggedIn) {
-        throw redirect('/login?message=You must be logged in to view this page')
+        throw redirect(
+            `/login?message=You must be logged in to view this page&redirectTo=${pathname}`
+        )
     }
 }
